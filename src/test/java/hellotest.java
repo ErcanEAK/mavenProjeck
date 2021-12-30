2c59eab07994fd2aa6df963c8cdca7bb919891e6
ErcanEAK <ercanakkaya86@hotmail.com>
2021-12-31 01:46:56 +0300

    update more hello feature

--- src/test/java/hellotest.java
+++ src/test/java/hellotest.java
@@ -2,5 +2,7 @@ public class hellotest {
     public static void main(String[] args) {
         System.out.println( "HellosTest");
         System.out.println("developing hello future");
+        System.out.println("Github repo is alreday published to github");
+        System.out.println(" doing more work");
     }
 }
