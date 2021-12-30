a69615488f4adadc7b0259ecb6cb9674584beb55
ErcanEAK <ercanakkaya86@hotmail.com>
2021-12-31 01:38:39 +0300

    started hello future

--- src/test/java/hellotest.java
+++ src/test/java/hellotest.java
@@ -1,6 +1,6 @@
 public class hellotest {
     public static void main(String[] args) {
         System.out.println( "HellosTest");
-
+        System.out.println("developing hello future");
     }
 }
